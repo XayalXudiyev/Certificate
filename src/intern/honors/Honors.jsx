9f994fac0465daba1db1e94afce6c021819c3645
XayalXudiyev <xeyalxeyal068@gmail.com>
2024-03-07 17:14:24 +0400

    honor certificate completed

--- src/intern/honors/Honors.jsx
+++ src/intern/honors/Honors.jsx
@@ -2,54 +2,46 @@ import React from 'react'
 import HonorsBG from '/intern/honors.svg'
 import EtibarM from '/signatures/EtibarM.svg'
 
-
 const Honors = () => {
   return (
-    <div className='relative flex items-center justify-center'>
-      <img src={HonorsBG} alt="" className='  print:w-full  ' />
+    <div className='relative flex items-center justify-center flex-col text-[#0f4085]  '>
+      <img src={HonorsBG} alt="" className='print:w-full  ' />
 
-      <div className='flex flex-col absolute w-2/5  print:w-5/6  print:-left-5 print:top-44 print:bottom-2/4 print:pt'>
+      <div className='flex flex-col absolute mt-32 w-[40%] left-[340px] pr-2 h-fit print:left-[60px] print:pr-0 print:w-[72%]' >
 
-        <textarea rows='1' className=' mt-56 font-kunstler text-[#20497d] text-7xl w-full text-center focus:outline-none mx-auto print:text-[40px] print:m-0 print:text-end  print:absolute'>Gurban Guliyev</textarea>
+        <p className='text-end mb-2 text-xl lead4'>This is to certify that</p>
 
+        <textarea rows='1' className='text-end font-kunstler text-[#20497d] text-[90px] w-full -mb-5 focus:outline-none mx-auto '>Gurban Guliyev</textarea>
 
-        <textarea className='print:text-[8px] print:text-end print:leading-[6px] print:mt-11 '>has successfully completed 3 months Data Science Internship at</textarea>
-        <textarea className='print:text-[8px] print:text-end print:leading-[6px]'>QSS Analytics onOctober 14 , 2023</textarea>
-        <textarea rows={3} className='print:absolute  print:text-[7px] print:tracking-wider print:w-[76%] print:ml-12 print:top-[85px]'                                                                                                                                                                                                                                                                                                                                                                  >
+        <textarea rows={1} className='focus:outline-none text-end  text-lg'>has successfully completed 3 months Data Science Internship at</textarea>
+        <textarea rows={1} className='focus:outline-none text-end -mt-1 text-lg'>QSS Analytics onOctober 14 , 2023</textarea>
+        <textarea rows={3} className='focus:outline-none tracking-wider mt-14 leading-5 print:w-[87%] '>
           Special note: During working process, successfully completion
           of most assigned tasks and his positive habits and qualities
           enable Gurban to be a professional expert in the future
         </textarea>
-        <div className='print:absolute flex flex-col gap-1  print:text-[7px] print:tracking-wider print:w-[76%] print:ml-12 print:top-32 print:leading-[5px]'>
-          Accomplished programs during internship:
-          <textarea rows="1" className='print:pb-[2px] '>•  Data Science for Python</textarea>
-          <textarea rows="1" className='print:pb-[2px] '>•  Machine Learning with Python</textarea>
-          <textarea rows="1" className='print:pb-[2px] '>•  Deep Learning with Python</textarea>
-          <textarea rows="1" className='print:pb-[2px] '>•  SQL for Data Science</textarea>
-          <textarea rows="1" className='print:pb-[2px] '>•  Introduction to Big Data</textarea>
-          <textarea rows="1" className='print:pb-[2px] '>•  Data Analytics with Excel</textarea>
-          <textarea rows="1" className='print:pb-[2px] '></textarea>
-          <textarea rows="1" className='print:pb-[2px] '></textarea>
-          <textarea rows="1" className='print:pb-[2px] '></textarea>
-          <textarea rows="1" className='print:pb-[2px] '></textarea>
-
+        <div className=' flex flex-col   text-start mt-7 '>
+          <p>Accomplished programs during internship:</p>          <textarea rows="1" className='focus:outline-none leading-4'>•  Data Science for Python</textarea>
+          <textarea rows="1" className='focus:outline-none leading-6'>•  Machine Learning with Python</textarea>
+          <textarea rows="1" className='focus:outline-none leading-6'>•  Deep Learning with Python</textarea>
+          <textarea rows="1" className='focus:outline-none leading-6'>•  SQL for Data Science</textarea>
+          <textarea rows="1" className='focus:outline-none leading-6'>•  Introduction to Big Data</textarea>
+          <textarea rows="1" className='focus:outline-none leading-6'>•  Data Analytics with Excel</textarea>
+          <textarea rows="1" className='focus:outline-none leading-6'></textarea>
+          <textarea rows="1" className='focus:outline-none leading-6'></textarea>
+          <textarea rows="1" className='focus:outline-none leading-6'></textarea>
+          <textarea rows="1" className='focus:outline-none leading-6'></textarea>
         </div>
-
-
-
       </div>
-        <div className='flex   text-start   print:absolute print:bottom-3  print:left-5'>
-          <div className='pt- relative '>
-            <img src={EtibarM} alt="" className='print:absolute print:bottom-2 print:m-6' />
 
-            <hr className='border-black  print:border print:border-t-0' />
+      <div className='absolute bottom-9 left-[26%] print:left-[60px] print:bottom-14'>
+        <img src={EtibarM} alt="" className=' m-4 -mb-2 print:m-0' />
 
-            <p className='font-medium print:text-[10px]'>Etibar Huseynli</p>
-            <p className='font-medium print:text-[10px]'>Director of QSS Analytics</p>
-          </div>
+        <hr className='border-black  border border-t-0' />
 
-        </div>
-
+        <p className='uppercase tracking-wide '>Etibar Huseynli</p>
+        <p className='text-start mt-2 leading-5'>Director of QSS <br /> Analytics</p>
+      </div>
 
     </div>
   )
--- src/intern/normal/Normal.jsx
+++ src/intern/normal/Normal.jsx
@@ -5,39 +5,47 @@ import EtibarM from '/signatures/EtibarM.svg'
 
 const Normal = () => {
     return (
-        <div className='flex justify-center items-center relative '>
-            <img src={NormalBG} alt="" className='block ' />
-            <div className='flex flex-col absolute bg-slate-500'>
-                <textarea rows='1' className='   font-kunstler text-[#20497d]   text-8xl  text-center focus:outline-none w-3/4 mx-auto'>Khayal Khudiyev</textarea>
-                <textarea>has successfully completed 3 months Data Science Internship at</textarea>
-                <textarea>QSS Analytics onOctober 14 , 2023</textarea>
-                <textarea rows={3}>
-                    Special note: During working process, successfully completion
-                    of most assigned tasks and his positive habits and qualities
-                    enable Gurban to be a professional expert in the future
-                </textarea>
-                <textarea>
-                    Accomplished programs duringinternship:
-                    • Data Science for Python
-                    • Machine Learning with Python
-                    • Deep Learning with Python
-                    • SQL for Data Science
-                    • İntroduction to Big Data
-                    • Data Analytics with Excel
-                </textarea>
-                {/* <div className='flex gap-16 text-start px-8'>
-                    <div className='pt-2 relative'>
-                        <img src={EtibarM} alt="" className='absolute bottom-6 m-2' />
-
-                        <hr className='' />
-
-                        <p className='font-medium text-lg'>Etibar Huseynli</p>
-                    </div>
-
-                </div> */}
+        <div className='relative flex items-center justify-center flex-col text-[#0f4085] '>
+          <img src={NormalBG} alt="" className='print:w-full  ' />
+    
+          <div className='flex flex-col absolute mt-32 w-[40%] left-[340px] pr-2 h-fit print:left-[60px] print:pr-0 print:w-[72%]' >
+    
+            <p className='text-end mb-4 text-xl lead4'>This is to certify that</p>
+    
+            <textarea rows='1' className='text-end font-kunstler text-[#20497d] text-[90px] w-full -mb-5 focus:outline-none mx-auto '>Gurban Guliyev</textarea>
+    
+            <textarea rows={1} className='focus:outline-none text-end  text-lg'>has successfully completed 3 months Data Science Internship at</textarea>
+            <textarea rows={1} className='focus:outline-none text-end -mt-1 text-lg'>QSS Analytics onOctober 14 , 2023</textarea>
+            <textarea rows={3} className='focus:outline-none tracking-wider mt-14 leading-5 print:w-[87%] '>
+              Special note: During working process, successfully completion
+              of most assigned tasks and his positive habits and qualities
+              enable Gurban to be a professional expert in the future
+            </textarea>
+            <div className=' flex flex-col   text-start mt-7 '>
+              <p>Accomplished programs during internship:</p>          <textarea rows="1" className='focus:outline-none leading-4'>•  Data Science for Python</textarea>
+              <textarea rows="1" className='focus:outline-none leading-6'>•  Machine Learning with Python</textarea>
+              <textarea rows="1" className='focus:outline-none leading-6'>•  Deep Learning with Python</textarea>
+              <textarea rows="1" className='focus:outline-none leading-6'>•  SQL for Data Science</textarea>
+              <textarea rows="1" className='focus:outline-none leading-6'>•  Introduction to Big Data</textarea>
+              <textarea rows="1" className='focus:outline-none leading-6'>•  Data Analytics with Excel</textarea>
+              <textarea rows="1" className='focus:outline-none leading-6'></textarea>
+              <textarea rows="1" className='focus:outline-none leading-6'></textarea>
+              <textarea rows="1" className='focus:outline-none leading-6'></textarea>
+              <textarea rows="1" className='focus:outline-none leading-6'></textarea>
             </div>
+          </div>
+    
+          <div className='absolute bottom-9 left-[26%] print:left-[60px] print:bottom-14'>
+            <img src={EtibarM} alt="" className=' m-4 -mb-2 print:m-0' />
+    
+            <hr className='border-black  border border-t-0' />
+    
+            <p className='uppercase tracking-wide '>Etibar Huseynli</p>
+            <p className='text-start mt-2 leading-5'>Director of QSS <br /> Analytics</p>
+          </div>
+    
         </div>
-    )
+      )
 }
 
 export default Normal 
